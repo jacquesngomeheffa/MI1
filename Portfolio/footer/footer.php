aa20a19414b0738de2d3d5910e39b1932d1fe222
jacquesngomeheffa <jacques.ngomeheffa@student.odisee.be>
2016-12-23 18:03:03 +0100

    Add files via upload

---
+++ Portfolio/footer/footer.php
@@ -0,0 +1,55 @@
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<div class="footer-wrap">
+	<div class="footer">
+		<div class="bolg">
+            <div class="title">
+            	<h1>Buc Motor Cameroun</h1>
+            </div>
+        	<div class="panel mar-right115">
+            <div class="content">
+            	<ul>
+                	<li><img src="images/icon1.png" alt="icon" /></li>
+                    <li>Buc motor<br />
+                  Août 2016</li>
+            	</ul>
+                <p>Buc est une entreprise de vente et de réparation de motos.</p>
+                <p><a href="#">Plus d'info + </a></p>
+            </div>
+           </div> 
+           
+           <div class="panel">
+            <div class="content">
+            	<ul>
+                	<li><img src="images/icon1.png" alt="icon" /></li>
+                    <li>Buc motor<br />
+Août 2016</li>
+            	</ul>
+                <p>Possibilité  de commande en ligne&nbsp;: Pièces de rechanges, etc …</p>
+                <p><a href="#">Plus d'info + </a></p>
+            </div>
+           </div>
+        </div>
+        
+        <div class="quickcontact">
+        	<div class="title">
+            	<h1> Contact Buc Motor</h1>
+            </div>
+			<div class="panel">
+            <div class="content">
+              <p>Buc Motor: A  proximité de 
+              Bepanda (Douala- Camaroun)</p>
+                <p><span>Tel:</span> (+237) 670 009 747 /  696 173 629</p>
+                <p><span>Email :</span> bucmotor16@yahoo.fr</p>
+            </div>
+           </div>
+        </div>
+    </div>
+</div><!---footer--->
+<div class="copyright-wrap">
+	<div class="copyright">
+    	<div class="content">
+        	<p>© 2012 All Rights Reserved  |  Privacy Policy   
+            Designed by :<a href="http://www.Bucmotor.com" class="active"> www.Bucmotor.com</a></p>
+        </div>
+    </div>
+</div><!---copyright-wrap--->
