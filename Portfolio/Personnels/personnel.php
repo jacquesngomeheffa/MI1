1e4cfb218e5b312ea8e00cf81a4e7094b3bfa09c
jacquesngomeheffa <jacques.ngomeheffa@student.odisee.be>
2017-08-24 18:48:22 +0200

    Add files via upload

---
+++ Portfolio/Personnels/personnel.php
@@ -0,0 +1,35 @@
+<link href="../css/styles.css" rel="stylesheet" type="text/css" />
+        	<div class="title">
+        	 <h1 class="align">LE PERSONNELS</h1></div>
+            	<div class="panel">
+                    <img src="images/image1.png" alt="David Urbain PDG, Manager" />
+                    <div class="content">
+                    <p>David Urbain </p>
+                   	  <p>PDG, Manager </p>
+                        <div class="button"><a href="#">More</a></div>
+                    </div>
+                </div>
+                <div class="panel">
+                    <img src="images/image2.png" alt="Biatho Rebecca Support Manager" />
+                    <div class="content">
+                    <p>Biatho Rebecca </p>
+                   	  <p>Support Manager</p>
+                        <div class="button"><a href="#">More</a></div>
+                    </div>
+                </div>
+                <div class="panel">
+                    <img src="images/image3.png" alt="Ntonga Alain Gérant-Mécanicien" />
+                    <div class="content">
+                    <p>Ntonga Alain </p>
+                   	  <p>Gérant-Mécanicien</p>
+                        <div class="button"><a href="#">More</a></div>
+                    </div>
+            </div>
+                      
+                     <div class="panel bor-bottm-non">
+                    <img src="images/ose.png" alt="Dikein long Osé Comptable" />
+                    <div class="content">
+                    <p>Dikein long Osé </p>
+                   	  <p>Comptable</p>
+                        <div class="button"><a href="#">More</a></div>
+            </div>
